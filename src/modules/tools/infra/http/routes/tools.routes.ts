a6d35883ae3c151ecbc89b5b6ff4ba4484f736e0
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-07-04 04:19:38 -0300

    feat: add tools routes validation

--- src/modules/tools/infra/http/routes/tools.routes.ts
+++ src/modules/tools/infra/http/routes/tools.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import ToolsController from '../controllers/ToolsController';
 import ensureAuthenticated from '../../../../users/infra/http/middlewares/ensureAuthenticated';
 
@@ -8,8 +10,41 @@ const toolsController = new ToolsController();
 toolsRouter.use(ensureAuthenticated);
 
 toolsRouter.get('/', toolsController.index);
-toolsRouter.post('/', toolsController.create);
-toolsRouter.put('/:id', toolsController.update);
-toolsRouter.delete('/:id', toolsController.delete);
+toolsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      title: Joi.string().required(),
+      link: Joi.string().required(),
+      description: Joi.string().required(),
+      tags: Joi.array().items(Joi.string()).required(),
+    },
+  }),
+  toolsController.create,
+);
+toolsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid({ version: 'uuidv4' }).required(),
+    },
+    [Segments.BODY]: {
+      title: Joi.string().required(),
+      link: Joi.string().required(),
+      description: Joi.string().required(),
+      tags: Joi.array().items(Joi.string()).required(),
+    },
+  }),
+  toolsController.update,
+);
+toolsRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid({ version: 'uuidv4' }).required(),
+    },
+  }),
+  toolsController.delete,
+);
 
 export default toolsRouter;
