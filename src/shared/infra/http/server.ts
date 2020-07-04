cb193c2aa84159ec7fde5e974c7d894e0cc75ec2
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-07-04 00:36:59 -0300

    feat: add cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
+import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
@@ -12,6 +13,7 @@ import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
